33a24b5a3a1e5984592a8fda3a00c58385b6f043
shamaz332 <shamazsaeed332@gmail.com>
2020-10-20 12:45:02 +0500

    api set

---
+++ src/components/Row.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Row({props}) {
+  return (
+    <div>
+      {/* title*/}
+  <h2>{props.title}</h2>
+      {/* container */}
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+  );
+}
+
+export default Row;
