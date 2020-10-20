1bb586f2acab9ffd97433a87be8aa5be90b58ae6
shamaz332 <shamazsaeed332@gmail.com>
2020-10-20 16:32:10 +0500

    get all movies

--- src/App.js
+++ src/App.js
@@ -11,9 +11,13 @@ function App() {
 
 
 <Row title="NETFLIX ORIGNALS" fetchUrl={requests.fetchNetflixOriginals}/>
-
 <Row title="TRENDING NOW"fetchUrl={requests.fetchTrending}/>
-
+<Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+<Row title="Romance Movies"fetchUrl={requests.fetchRomanceMovies}/>
+<Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+<Row title="Comedy Movies"fetchUrl={requests.fetchComedyMovies}/>
+<Row title="Documentry Movies"fetchUrl={requests.fetchDocumentaries}/>
+<Row title="Horror Movies"fetchUrl={requests.fetchHorrorMovies}/>
 
   </div>;
 }
--- src/components/ MoviesList/Row.js
+++ src/components/ MoviesList/Row.js
@@ -1,37 +1,38 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import instance from "../movies/getReq";
-import "./Row.css"
+import "./Row.css";
 
-const base_url="https://image.tmdb.org/t/p/original/"
+const base_url = "https://image.tmdb.org/t/p/original/";
 
-
-
-
-function Row({title,fetchUrl}) {
+function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
-useEffect(()=>{
-    async function fetchData(){
-        const request = await instance
-        .get(fetchUrl)
-        console.log(request.data.results)
-       setMovies(request.data.results)
-        return request
+  useEffect(() => {
+    async function fetchData() {
+      const request = await instance.get(fetchUrl);
+      console.log(request.data.results);
+      setMovies(request.data.results);
+      return request;
     }
 
-    fetchData()
-},[fetchUrl])
+    fetchData();
+  }, [fetchUrl]);
   return (
     <div className="row">
       {/* title*/}
       <h2>{title}</h2>
       {/* container */}
       <div className="row_post_parent">
-{movies.map((movie)=>(
-    <img className="row_post" src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
-))}
+        {movies.map((movie) => (
+          <img
+          key={movie.id}
+            className="row_post"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
       </div>
     </div>
-  ); 
+  );
 }
 
 export default Row;
